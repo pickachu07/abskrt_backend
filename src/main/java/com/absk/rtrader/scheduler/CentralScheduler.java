b2a55bee8a66cd1595340a6e48f336f256eba869
Subhodip Kumar <subhodip.kumar.7@gmail.com>
2019-03-23 11:53:07 +0530

    added central scheduler with start stop functionality

---
+++ src/main/java/com/absk/rtrader/scheduler/CentralScheduler.java
@@ -0,0 +1,64 @@
+package com.absk.rtrader.scheduler;
+
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.CronTrigger;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@EnableScheduling
+@ComponentScan
+@Component
+public class CentralScheduler {
+
+	private static AnnotationConfigApplicationContext CONTEXT = null;
+
+    @Autowired
+    private ThreadPoolTaskScheduler scheduler;
+
+    public static CentralScheduler getInstance() {
+        if (!isValidBean()) {
+            CONTEXT = new AnnotationConfigApplicationContext(CentralScheduler.class);
+        }
+
+        return CONTEXT.getBean(CentralScheduler.class);
+    }
+
+    @Bean
+    public ThreadPoolTaskScheduler taskScheduler() {
+        return new ThreadPoolTaskScheduler();
+    }
+
+    public void start(Runnable task, String scheduleExpression) throws Exception {
+        scheduler.schedule(task, new CronTrigger(scheduleExpression));
+    }
+
+    public void start(Runnable task, Long delay) throws Exception {
+        scheduler.scheduleWithFixedDelay(task, delay);
+    }
+
+    public void stopAll() {
+        scheduler.shutdown();
+        CONTEXT.close();
+    }
+
+    private static boolean isValidBean() {
+        if (CONTEXT == null || !CONTEXT.isActive()) {
+            return false;
+        }
+
+        try {
+            CONTEXT.getBean(CentralScheduler.class);
+        } catch (NoSuchBeanDefinitionException ex) {
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/main/java/com/absk/rtrader/scheduler/tasks/MainTask.java
@@ -0,0 +1,11 @@
+package com.absk.rtrader.scheduler.tasks;
+
+public class MainTask implements Runnable{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("Main Task");
+	}
+
+}
