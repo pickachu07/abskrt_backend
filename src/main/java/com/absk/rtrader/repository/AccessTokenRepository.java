5cf4e4af0d8942e32d35bc207ce6306db10b373a
pikachu07 <subhodip.kumar.7@gmail.com>
2019-05-28 18:15:30 +0530

    removed find by date function from accesstoken repo

--- src/main/java/com/absk/rtrader/repository/AccessTokenRepository.java
+++ src/main/java/com/absk/rtrader/repository/AccessTokenRepository.java
@@ -1,14 +1,15 @@
 package com.absk.rtrader.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.absk.rtrader.model.AccessToken;
+import com.github.rishabh9.riko.upstox.login.models.AccessToken;
 
 @Repository
 public interface AccessTokenRepository extends MongoRepository<AccessToken, Long>{
 
-	public List<AccessToken> getByDate(String string);
+	//public List<AccessToken> getByDate(String string);
+
+	@SuppressWarnings("unchecked")
+	public AccessToken save(AccessToken accessToken);
 }
