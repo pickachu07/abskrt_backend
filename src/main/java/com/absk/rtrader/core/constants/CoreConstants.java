33f04e6200e36a8c5232ffc7d2fefd251814ab3d
pikachu07 <subhodip.kumar.7@gmail.com>
2019-05-28 18:11:19 +0530

    added coreconstants

---
+++ src/main/java/com/absk/rtrader/core/constants/CoreConstants.java
@@ -0,0 +1,7 @@
+package com.absk.rtrader.core.constants;
+
+public class CoreConstants {
+
+	public static final String FRONTEND_SETTINGS_URI = "http://localhost:3000/settings";
+	public static final String FRONTEND_BASE_URI = "http://localhost:3000";
+}
