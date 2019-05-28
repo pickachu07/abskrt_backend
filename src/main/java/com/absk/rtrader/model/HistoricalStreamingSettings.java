6974d8dd8c24fd532a75dd3d971509880d8ae37d
pikachu07 <subhodip.kumar.7@gmail.com>
2019-05-28 18:16:10 +0530

    refactoring: removing unused code

--- src/main/java/com/absk/rtrader/model/HistoricalStreamingSettings.java
+++ src/main/java/com/absk/rtrader/model/HistoricalStreamingSettings.java
@@ -1,21 +1,18 @@
 package com.absk.rtrader.model;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class HistoricalStreamingSettings {
 
 	private String ticker_name;
 	private int brick_size;
-	private Date date;
 	
 	public HistoricalStreamingSettings(String ticker_name, int brick_size,String date) throws ParseException {
 		super();
 		this.ticker_name = ticker_name;
 		this.brick_size = brick_size;
-		SimpleDateFormat dateFormatter=new SimpleDateFormat("dd-mm-yyyy");
-		this.date =dateFormatter.parse(date);
+		//SimpleDateFormat dateFormatter=new SimpleDateFormat("dd-mm-yyyy");
+		//this.date =dateFormatter.parse(date);
 	}
 	public HistoricalStreamingSettings(String ticker_name, int brick_size){
 		super();
