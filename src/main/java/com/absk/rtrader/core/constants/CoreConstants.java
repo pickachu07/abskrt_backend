14878d966478cb61ae9b45e2fecdf7618643d815
pikachu07 <subhodip.kumar.7@gmail.com>
2019-09-17 11:43:15 +0530

    adding some more core constants for async workers

--- src/main/java/com/absk/rtrader/core/constants/CoreConstants.java
+++ src/main/java/com/absk/rtrader/core/constants/CoreConstants.java
@@ -1,7 +1,13 @@
 package com.absk.rtrader.core.constants;
 
+
 public class CoreConstants {
 
 	public static final String FRONTEND_SETTINGS_URI = "http://localhost:3000/settings";
 	public static final String FRONTEND_BASE_URI = "http://localhost:3000";
+	public static final int CORE_POOL_SIZE = 20;
+	public static final int MAX_POOL_SIZE = 100;
+	public static final String ASYNC_THREAD_NAME_PREFIX = "Async-";
+	
+	
 }
