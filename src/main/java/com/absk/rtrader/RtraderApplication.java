a1e077fdc7cee7bf17e419741bdc3117c8bc808f
pikachu07 <subhodip.kumar.7@gmail.com>
2019-09-17 11:42:28 +0530

    added async worker config

--- src/main/java/com/absk/rtrader/RtraderApplication.java
+++ src/main/java/com/absk/rtrader/RtraderApplication.java
@@ -2,13 +2,34 @@ package com.absk.rtrader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import com.absk.rtrader.core.constants.CoreConstants;
 
 
 @SpringBootApplication
 @EnableScheduling
-public class RtraderApplication {
-
+@EnableAsync
+public class RtraderApplication {	
+	/*
+	 * Configuring default Async Task Executor
+	 */
+	@Bean("AsyncTaskExecuter")
+	public TaskExecutor getAsyncExecuter() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(CoreConstants.CORE_POOL_SIZE);
+		executor.setMaxPoolSize(CoreConstants.MAX_POOL_SIZE);
+		executor.setWaitForTasksToCompleteOnShutdown(true);
+		executor.setThreadNamePrefix(CoreConstants.ASYNC_THREAD_NAME_PREFIX);
+		return executor;
+	}
+	/*
+	 * Entrypoint of the Application - Spring boot style. Part of boilerplate.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(RtraderApplication.class, args);
 	}
