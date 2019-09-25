c440e2cd3b7168eb5f6597ca8afa1f992c674cdb
pikachu07 <subhodip.kumar.7@gmail.com>
2019-09-26 01:02:52 +0530

    added strike type constants

---
+++ src/main/java/com/absk/rtrader/exchange/upstox/constants/UpstoxStrikeTypeConstants.java
@@ -0,0 +1,9 @@
+package com.absk.rtrader.exchange.upstox.constants;
+
+public class UpstoxStrikeTypeConstants {
+
+	
+	public static final String PUT  = "put";
+	public static final String CALL = "call";
+	
+}
